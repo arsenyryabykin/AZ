e9598e30a4d4a77c6ea4f8e1ebf7fc70d9635434
arsenyryabykin@git config --global user.name arsenyryabykin <arsenyryabykin@yandex.ru>
2025-01-02 12:51:12 +0300

    прежняя версия файла

--- ShelfPainter/shelf_painter.py
+++ ShelfPainter/shelf_painter.py
@@ -1,48 +1,64 @@
 import sys
 import pygame
 from config import W, H
-
 from shelf import make_shelf
 
 class App:
-
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((W, H), pygame.FULLSCREEN)
-
-        self.clock = pygame.time.Clock()
-
+        #self.screen = pygame.display.set_mode((W, H), pygame.SCALED)
+        self.surf = pygame.Surface((W, H))
+        # self.clock = pygame.time.Clock()
 
     def run(self):
+
         print("App running")
+        shelf_1 = make_shelf(self.surf, 1)      # Создание объекта стеллажа
+        shelf_2 = make_shelf(self.surf, 2)      # Создание объекта стеллажа
+        shelf_3 = make_shelf(self.surf, 3)      # Создание объекта стеллажа
+
+        # Цикл приложения
+        # while True:
+            # for event in pygame.event.get():
+            #     #pos = pygame.mouse.get_pos()
+            #
+            #     if event.type == pygame.QUIT:
+            #         pygame.quit()
+            #         sys.exit()
+            #
+            #     if(event.type == pygame.KEYDOWN):
+            #         if(event.key == pygame.K_q):
+            #             pygame.quit()
+            #             sys.exit()
+
+                    # elif(event.key == pygame.K_s):
+                    #     pygame.image.save(self.screen, "screenshot.png")
+
+            # Отрисовка стеллажей ##############################################
+
+        self.surf.fill((255,255,255))
+        for cell in shelf_1:
+            cell.draw_hex_area(self.surf)
+        pygame.image.save(self.surf, "shelf_1.png")
 
 
-        shelf = make_shelf(self.screen)
-        print(shelf)
+        self.surf.fill((255, 255, 255))
+        for cell in shelf_2:
+            cell.draw_hex_area(self.surf)
+        pygame.image.save(self.surf, "shelf_2.png")
 
+        self.surf.fill((255, 255, 255))
+        for cell in shelf_3:
+            cell.draw_hex_area(self.surf)
+        pygame.image.save(self.surf, "shelf_3.png")
 
-        while True:
-            for event in pygame.event.get():
-                pos = pygame.mouse.get_pos()
+        print("App finished")
+        pygame.quit()
+        sys.exit()
 
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if(event.type == pygame.KEYDOWN):
-                    if(event.key == pygame.K_q):
-                        pygame.quit()
-                        sys.exit()
-                    elif(event.key == pygame.K_s):
-                        pygame.image.save(self.screen, "screenshot.png")
-
-            ##### Отрисовка ########################################################
-            self.screen.fill((255,255,255))
-            for cell in shelf:
-                cell.draw_hex_area(self.screen)
             #######################################################################
 
-            pygame.display.update()
-            self.clock.tick(30)
+            # pygame.display.update()
 
 
 if __name__ == '__main__':
